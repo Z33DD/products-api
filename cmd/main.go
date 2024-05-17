66f24a2d079db5003eed64c37ab2062eb334aa7b
Pedro <pedro@z33dd.com>
2024-05-17 10:43:17 -0300

    Update docker config

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
-import ctrl "eulabs/internal/controller"
+import (
+	ctrl "eulabs/internal/controller"
+	"eulabs/internal/service"
+)
 
 func main() {
+	service.GetDatabase()
 	server := ctrl.RouterFactory()
 
 	server.Logger.Fatal(server.Start(":1323"))
